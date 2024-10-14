1372a63ea40cfe6cb811c76b25a68052cf06b0c6
Lukman Sanusi <lckmnzans@gmail.com>
2024-10-14 13:48:26 +0700

    change route name

--- router/route.js
+++ router/route.js
@@ -9,7 +9,7 @@ router.post('/', (req,res) => {
     res.send('POST request to the homepage')
 })
 
-const userRoute = require('../controller/userController');
-router.use('/api/v1/users', userRoute);
+const userRoute = require('../controller/accountController');
+router.use('/api/v1/', userRoute);
 
 module.exports = router;
