0b53bebbd4f17c6bb685a74c2139eb6bc4d7ad1a
Lukman Sanusi <lckmnzans@gmail.com>
2024-10-15 16:01:20 +0700

    implement get list of document and document download

--- controller/documentController.js
+++ controller/documentController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+const File = require('../model/File');
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, 'uploads/')
@@ -20,7 +21,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({ storage: storage, fileFilter: fileFilter });
 
 router.post('/document', (req,res) => {
-    upload.single('document')(req, res, (err) => {
+    upload.single('document')(req, res, async (err) => {
         if (err) {
             if (err.message == 'WrongFileType') {
                 return res.status(400).json({ message:'File type not accepted. Only PDF accepted' });
@@ -32,11 +33,48 @@ router.post('/document', (req,res) => {
             return res.status(400).json({ message:'No file uploaded' });
         }
 
+        const fileData = new File({
+            filename: req.file.filename,
+            path: req.file.path
+        })
+        const savedFile = await fileData.save();
+
         res.json({
             message:'File uploaded succesfully',
-            file: req.file
+            file: savedFile
         })
     });
-})
+});
 
+router.get('/document/:filename', (req,res) => {
+    File.findOne({ filename: req.params.filename })
+    .then((file) => {
+        if (!file) {
+            return res.status(404).json({ message:'File not found' });
+        } else {
+            res.download(file.path, req.params.filename+".pdf", (err) => {
+                if (err) {
+                    res.status(500).json({
+                        error: err,
+                        message: err.message
+                    })
+                }
+            });
+        }
+    })
+    .catch(() => {
+        res.status(404).json({ message: 'File cannot be retrieved' });
+    });
+});
+
+
+router.get('/list-document', (req,res) => {
+    File.find({})
+    .then((files) => {
+        return res.status(200).json(files);
+    })
+    .catch((err) => {
+        return res.status(500).json({ message: err.message });
+    })
+})
 module.exports = router;
