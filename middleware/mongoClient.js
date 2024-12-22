715bc8d512f0b05a9fbf2b185505a1abfab2131e
Lukman Sanusi <lckmnzans@gmail.com>
2024-12-22 21:54:17 +0700

    mongoClient add authSource

--- middleware/mongoClient.js
+++ middleware/mongoClient.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const url = require('../config/keys').mongoUri;
 
 module.exports = async function() {
-    mongoose.connect(url+"/test")
+    mongoose.connect(url+"/test?authSource=admin")
     .then(() => console.log('\x1b[36m%s\x1b[0m', 'MongoDB connected'))
     .catch((err) => console.log(err));
 };
