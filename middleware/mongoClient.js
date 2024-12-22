07fb039729e39c89cada4e4436f9d38e0729e810
Lukman Sanusi <lckmnzans@gmail.com>
2024-12-22 21:35:31 +0700

    mongoClient change catch

--- middleware/mongoClient.js
+++ middleware/mongoClient.js
@@ -4,5 +4,5 @@ const url = require('../config/keys').mongoUri;
 module.exports = function() {
     mongoose.connect(url+"/test")
     .then(() => console.log('\x1b[36m%s\x1b[0m', 'MongoDB connected'))
-    .catch(err => console.error);
+    .catch((err) => console.log(err));
 };
