bb198861152140c1b2f9db1d55dc98643855996c
Lukman Sanusi <lckmnzans@gmail.com>
2024-12-22 22:08:35 +0700

    add timeout

--- index.js
+++ index.js
@@ -41,6 +41,11 @@ const router = require('./router/route');
 app.use(express.static('public'));
 app.use('/', router);
 
+app.use((req,res,next) => {
+    req.setTimeout(15000);
+    next();
+})
+
 // starting the server
 const swaggerDocs = require('./swagger');
 app.listen(port, hostname, () => {
