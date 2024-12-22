b48121b754edab7fd23f8bd2328fc65811523904
Lukman Sanusi <lckmnzans@gmail.com>
2024-12-22 23:42:36 +0700

    add cors config

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors({
     origin: 'https://frontend-nrc-production.up.railway.app',
-    methods: ['GET', 'POST', 'PATCH'],
+    methods: ['GET', 'POST', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
