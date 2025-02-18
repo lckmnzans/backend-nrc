a0ed12123f8a6b4371f05b50d6ca6bb6cb8f5ba6
Lukman Sanusi <lckmnzans@gmail.com>
2025-02-19 01:13:10 +0700

    resolve superuser not exists

--- middleware/mongoClient.js
+++ middleware/mongoClient.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 const url = require('../config/keys').mongoUri;
+const MongoUtils = require('../utils/MongoUtils');
 
 module.exports = async function() {
     mongoose.connect(url+"/test?authSource=admin")
-    .then(() => console.log('\x1b[36m%s\x1b[0m', 'MongoDB connected'))
+    .then((mongo) => { 
+        console.log('\x1b[36m%s\x1b[0m', 'MongoDB connected');
+        
+        mongoose.connection.on('connected', async () => {
+            MongoUtils.checkAndInsertUser();
+        });
+    })
     .catch((err) => console.log('\x1b[31m%s\x1b[0m', err));
 };
--- utils/MongoUtils.js
+++ utils/MongoUtils.js
@@ -5,37 +5,27 @@ const User = require('../model/User');
 class MongoUtils {
     static async checkAndInsertUser() {
         try {
-            await mongoose.connect(url+"/test");
-    
-            const collections = await mongoose.connection.db.listCollections({ name: 'users' }).toArray();
-            if (collections.length === 0) {
-                console.log('Collection does not exist. Skipping data insertion.');
-                return;
-            }
-    
             const existingUser = await User.findOne({ username: 'superuser', role: 'superadmin' });
             if (existingUser) {
                 console.log('Superuser already exists. Skipping data insertion.');
-                return
-            }
-    
-            const newUser = new User({
-                username: 'superuser',
-                email: 'superuser@mail.com',
-                role: 'superadmin'
-            })
-            User.register(newUser, "password123", (err,msg) => {
-                if (err) {
-                    console.log('Error: ', err);
-                } else {
-                    console.log('Superuser inserted successfully.');
-                }
-            });
+            } else {
+                console.log('Superuser does not exist. Inserting...');
+                const rootAccount = new User({
+                    username: 'superuser',
+                    email: 'superuser@mail.com',
+                    role: 'superadmin'
+                })
+                User.register(rootAccount, "password123", (err,msg) => {
+                    if (err) {
+                        console.log('Error registering a superuser: ', err);
+                    } else {
+                        console.log('Superuser inserted successfully.');
+                    }
+                });
+            };
         } catch (error) {
-            console.error('Error: ', error);
-        } finally {
-            mongoose.connection.close();
-        }
+            console.error('Error at checkAndInsertUser(): ', error);
+        };
     }
 }
 
